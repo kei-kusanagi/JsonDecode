8ec493a725b252b4774c54314d3a17c273f5acfe
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-23 14:13:56 -0600

    Initial Commit

---
+++ JsonDecode/JsonDecodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonDecodeApp.swift
+//  JsonDecode
+//
+//  Created by Juan Carlos Robledo Morales on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JsonDecodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JsonDecodeTests/JsonDecodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  JsonDecodeTests.swift
+//  JsonDecodeTests
+//
+//  Created by Juan Carlos Robledo Morales on 23/10/24.
+//
+
+import Testing
+@testable import JsonDecode
+
+struct JsonDecodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
